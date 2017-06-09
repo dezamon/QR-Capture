5552f42503674dabfada2248c90ca82a5d996166
taka29@me.com <taka@MacBook-Pro-3.local>
2017-06-09 10:56:31 +0900

    Pod fileを追加

---
+++ qrCapture/Pods/Target Support Files/Download-Font-iOS/Download-Font-iOS-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "DBLDownloadFont.h"
+
+FOUNDATION_EXPORT double Download_Font_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Download_Font_iOSVersionString[];
+
